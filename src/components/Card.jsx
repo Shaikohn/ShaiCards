19fc6f920ead3bc40b60cf0fb61ba4d2c356b2ec
unknown <shaicokohn@gmail.com>
2023-09-12 23:21:02 -0300

    Starting with Card design

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import './Card.css'
+
+const Card = () => {
+    return (
+        <div className="card contenedor">
+            <p className='ms-3'>Sports</p>
+            <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+            </div>
+            <p>@Tu vieja</p>
+        </div>
+    )
+}
+
+export default Card
