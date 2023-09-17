d1f8dd907fe5cf3fe6c0045bc2d35d10c48f21d5
unknown <shaicokohn@gmail.com>
2023-09-17 17:09:21 -0300

    User add card

--- src/components/Modals/CardModal.jsx
+++ src/components/Modals/CardModal.jsx
@@ -1,10 +1,68 @@
 import { TfiClose } from "react-icons/tfi";
 import Modals from "./Modals";
 import "./CardModal.css"
+import { useReducer, useState } from "react";
+import Swal from 'sweetalert2'
+import { useDispatch } from "react-redux"
+import { addUnconfirmedCard } from "../../redux/actions/unconfirmedCardActions";
 
+export default function CardModal({isOpenCardModal, closeCardModal}) {
 
-export default function CardModal({isOpenCardModal, closeCardModal, handleSubmit, handleChange, loading}) {
+    const [loading, setLoading] = useState(false)
+    const initialState = { type: '', user: '', text: '' }
+	const [cardData, setCardData] = useState(initialState)
+    const [reducerValue, forceUpdate] = useReducer((x) => x + 1, 0);
+    const dispatch = useDispatch()
 
+    const handleChange = (e) => {
+        setCardData({...cardData, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(cardData.text.length < 4) {
+            Swal.fire({
+                title: "Error",
+                text: "Debe tener un mínimo de 4 caracteres!",
+                icon: "error",
+                background: "#1a1a1a",
+                color: '#fff',
+                timer: 3000,
+            })
+        } else if(cardData.text.length > 200) {
+            Swal.fire({
+                title: "Error",
+                text: "Debe tener un máximo de 200 caracteres!",
+                icon: "error",
+                background: "#1a1a1a",
+                color: '#fff',
+                timer: 3000,
+            })
+        } else if(cardData.type.length < 1) {
+            Swal.fire({
+            title: "Error",
+            text: "Elige un tipo de carta!",
+            icon: "error",
+            background: "#1a1a1a",
+            color: '#fff',
+            timer: 3000,
+            })
+        } else if(cardData.user.length < 1) {
+            Swal.fire({
+            title: "Error",
+            text: "Escribe tu nombre o el que quieras!",
+            icon: "error",
+            background: "#1a1a1a",
+            color: '#fff',
+            timer: 3000,
+            })
+        }  else {
+            dispatch(addUnconfirmedCard(cardData, forceUpdate, e, setCardData, initialState, setLoading))
+        }
+    }
+
+    console.log(cardData)
+    
     return (
         <Modals isOpenModal={isOpenCardModal} closeModal={closeCardModal}>
 				<div className="d-flex">
@@ -19,15 +77,15 @@ export default function CardModal({isOpenCardModal, closeCardModal, handleSubmit
                         <form onSubmit={handleSubmit} noValidate>
                             <div className="mb-4" style={{justifyContent: 'space-between'}}>
                                 <div>
-                                    <input className="form-check-input" type="radio" name="typeOption" value="question" />
+                                    <input onChange={handleChange} className="form-check-input" type="radio" name="type" value="question" />
                                     <label className="labelMargin">Pregunta</label>
                                 </div>
                                 <div>
-                                    <input className="form-check-input" type="radio" name="typeOption" value="answer" />
+                                    <input onChange={handleChange} className="form-check-input" type="radio" name="type" value="answer" />
                                     <label className="labelMargin">Respuesta</label>
                                 </div>
                             </div>
-                            <input type="text" autoComplete='off' name="text" className="form-control nameInput" placeholder="Escribe tu nombre o el que quieras" onChange={handleChange} />
+                            <input type="text" autoComplete='off' name="user" className="form-control nameInput" placeholder="Escribe tu nombre o el que quieras" onChange={handleChange} />
 					        <textarea autoComplete='off' name="text" className="form-control textArea" placeholder="Escribe el contenido de la carta" onChange={handleChange} />
 						    <button style={{borderRadius: '5px', textDecoration: 'none'}} className="btn btn-success mt-2" type="submit">Enviar</button>
 					    </form>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,53 +2,10 @@ import Navbar from '../../components/Navbar/Navbar'
 import './Home.css'
 import { useModal } from '../../components/Modals/useModal'
 import CardModal from '../../components/Modals/CardModal'
-import { useState } from 'react'
-import Swal from 'sweetalert2'
 
 const Home = () => {
-
-  const initialState = { text: '', type: '' }
-	const [cardData, setCardData] = useState(initialState)
   const [isOpenCardModal, openCardModal, closeCardModal] = useModal(false);
-  const [loading, setLoading] = useState(false)
 
-  const handleChange = (e) => {
-    setCardData({...cardData, [e.target.name]: e.target.value})
-}
-
-const handleSubmit = (e) => {
-    e.preventDefault()
-    if(cardData.text.length < 4) {
-  Swal.fire({
-    title: "Error",
-    text: "Debe tener un mínimo de 4 caracteres!",
-    icon: "error",
-    background: "#1a1a1a",
-    color: '#fff',
-    timer: 3000,
-  })
-} else if(cardData.text.length > 200) {
-        Swal.fire({
-    title: "Error",
-    text: "Debe tener un máximo de 200 caracteres!",
-    icon: "error",
-    background: "#1a1a1a",
-    color: '#fff',
-    timer: 3000,
-  })
-} else if(cardData.type.length < 1) {
-        Swal.fire({
-    title: "Error",
-    text: "Elige una categoría!",
-    icon: "error",
-    background: "#1a1a1a",
-    color: '#fff',
-    timer: 3000,
-  })
-    } else {
-        /* dispatch(addAdminQuestion(questionData, forceUpdate, e, setQuestionData, initialState, setLoading)) */
-    }
-}
 
   return (
     <div>
@@ -59,7 +16,7 @@ const handleSubmit = (e) => {
       <button className='createButton'>Crear partida</button>
       <br />
       <button className='joinButton' onClick={openCardModal}>Sugerir nueva carta</button>
-      <CardModal isOpenCardModal={isOpenCardModal} closeCardModal={closeCardModal} handleSubmit={handleSubmit} handleChange={handleChange} loading={loading} />
+      <CardModal isOpenCardModal={isOpenCardModal} closeCardModal={closeCardModal} />
     </div>
   )
 }
